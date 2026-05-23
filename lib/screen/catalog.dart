dca7ba52d355ac1b8aa22e2d5fdc33bf65962864
diishqa <diana.34@list.ru>
2026-05-23 18:25:46 +0300

    catalog

---
+++ lib/screen/catalog.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import '../models/product.dart';
+import '../widgets/product_card.dart';
+
+class CatalogScreen extends StatelessWidget {
+  const CatalogScreen({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            padding: const EdgeInsets.all(20),
+            color: const Color(0xFFFFE2EB),
+            child: const Text(
+              'Каталог товаров',
+              style: TextStyle(
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+                color: Color(0xFFBD4367),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16),
+              child: GridView.builder(
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 16,
+                  mainAxisSpacing: 16,
+                  childAspectRatio: 0.7,
+                ),
+                itemCount: products.length,
+                itemBuilder: (context, index) {
+                  return ProductCard(product: products[index]);
+                },
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
